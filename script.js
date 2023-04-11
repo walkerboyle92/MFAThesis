48917ed4b8d0c5dcf3b2289fbf820f4886d0267c
Sondra Boyle <sb359120@ohio.edu>
2023-04-11 14:45:32 -0400

    got rid of window.onload

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-$(window).bind('load',function()  { 
-    let rank = localStorage.getItem("rank");
-    localStorage.setItem("rank", rank);
-    console.log("page loaded", rank);
- });
 
 $(document).ready(function(){
 
